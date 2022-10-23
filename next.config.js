5ac5f3044aff58af5be1c425f933cdc6052bc86c
hyunjoebrother <hyunjoe31@gmail.com>
2022-10-23 22:47:45 +0900

    feat: implement next.condfig.js image optim

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+moduel.exports = {
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+};
