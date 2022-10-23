2ad1e1e5ede0522493342cb57c77b2ff01dd87d2
hyunjoebrother <hyunjoe31@gmail.com>
2022-10-23 23:54:41 +0900

    feat: implement image iptimized

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+(module.exports = nextConfig),
+  {
+    images: {
+      loader: "akamai",
+      path: "/",
+    },
+  };
